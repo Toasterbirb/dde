28f53ac33488f590c2043af4a0919e5621af0563
Toasterbirb <git.53jvg@aleeas.com>
2021-09-11 19:27:50 +0300

    Color bar patch and customized default colors

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const unsigned int gappoh 	= 10; 		/* horizontal outer gap between window
 static const unsigned int gappov 	= 10; 		/* vertical outer gap between windows and screen edge */
 static const int smartgaps 			= 0; 		/* 1 means no outer gap when there is only one windows */
 
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -19,15 +19,21 @@ static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
 /* colors */
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char normfgcolor[]     = "#d8dee9";
+static const char normbgcolor[]     = "#3b4252";
+static const char selfgcolor[]      = "#2e3440";
+static const char nomrbordercolor[] = "#2E3440";
+static const char selbgcolor[]      = "#a3be8c";
+
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*           	    	fg         		bg         		border   */
+	[SchemeNorm] 		= { normfgcolor, 	normbgcolor, 	nomrbordercolor },
+	[SchemeSel]  		= { selfgcolor, 	selbgcolor, 	selbgcolor  },
+	[SchemeStatus]  	= { normfgcolor, 	normbgcolor,  	"#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
+	[SchemeTagsSel]  	= { selfgcolor, 	selbgcolor,  	"#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
+	[SchemeTagsNorm]  	= { normfgcolor, 	normbgcolor,  	"#000000"  }, // Tagbar left unselected {text,background,not used but cannot be empty}
+	[SchemeInfoSel]  	= { selfgcolor, 	selbgcolor,  	"#000000"  }, // infobar middle  selected {text,background,not used but cannot be empty}
+	[SchemeInfoNorm]  	= { normfgcolor, 	normbgcolor,  	"#000000"  }, // infobar middle  unselected {text,background,not used but cannot be empty}
 };
 
 /* tagging */
@@ -68,7 +74,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static Key keys[] = {
