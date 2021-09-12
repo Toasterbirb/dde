0f90b1ff1707cbc0b9b8a3576c1b8b205c5b0b7a
Toasterbirb <git.53jvg@aleeas.com>
2021-09-12 19:26:26 +0300

    Removed a debugging line and added a mouse bind for the layout menu

--- config.def.h
+++ config.def.h
@@ -153,7 +153,7 @@ static Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button1,        layoutmenu,     {0} },
 	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
