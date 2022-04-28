037d713a82d3688ab7eb01c83287ca36acd1c0cf
Toasterbirb <git@toasterbirb.com>
2022-04-28 17:53:02 +0300

    Added support for dual gpu + 4 monitors
    Take out -DDUAL_GPU if you don't need the weird hard coded dmenumon
    stuff

--- dwm/dwm.c
+++ dwm/dwm.c
@@ -57,6 +57,8 @@
 #define HEIGHT(X)               ((X)->h + 2 * (X)->bw)
 #define TAGMASK                 ((1 << LENGTH(tags)) - 1)
 #define TEXTW(X)                (drw_fontset_getwidth(drw, (X)) + lrpad)
+#define RIGHTOF(a,b)            (a.y_org > b.y_org) || \
+								((a.y_org == b.y_org) && (a.x_org > b.x_org))
 #define XRDB_LOAD_COLOR(R,V)    if (XrmGetResource(xrdb, R, NULL, &type, &value) == True) { \
                                   if (value.addr != NULL && strnlen(value.addr, 8) == 7 && value.addr[0] == '#') { \
                                     int i = 1; \
@@ -233,6 +235,9 @@ static void setup(void);
 static void seturgent(Client *c, int urg);
 static void showhide(Client *c);
 static void sigchld(int unused);
+#ifdef XINERAMA
+static void sortscreens(XineramaScreenInfo *screens, int n);
+#endif /* XINERAMA */
 static void sighup(int unused);
 static void sigterm(int unused);
 static void spawn(const Arg *arg);
@@ -1826,6 +1831,24 @@ sigchld(int unused)
 	while (0 < waitpid(-1, NULL, WNOHANG));
 }
 
+#ifdef XINERAMA
+void
+sortscreens(XineramaScreenInfo *screens, int n)
+{
+   int i, j;
+   XineramaScreenInfo *screen = ecalloc(1, sizeof(XineramaScreenInfo));
+
+   for (i = 0; i < n; i++)
+       for (j = i + 1; j < n; j++)
+           if (RIGHTOF(screens[i], screens[j])) {
+               memcpy(&screen[0], &screens[i], sizeof(XineramaScreenInfo));
+               memcpy(&screens[i], &screens[j], sizeof(XineramaScreenInfo));
+               memcpy(&screens[j], &screen[0], sizeof(XineramaScreenInfo));
+           }
+   XFree(screen);
+}
+#endif /* XINERAMA */
+
 void
 sighup(int unused)
 {
@@ -1844,7 +1867,31 @@ void
 spawn(const Arg *arg)
 {
 	if (arg->v == dmenucmd)
+#ifdef DUAL_GPU
+		switch(selmon->num)
+		{
+			case (0):
+				dmenumon[0] = '3';
+				break;
+
+			case (1):
+				dmenumon[0] = '1';
+				break;
+
+			case (2):
+				dmenumon[0] = '0';
+				break;
+
+			case (3):
+				dmenumon[0] = '2';
+				break;
+
+			default:
+				dmenumon[0] = '0' + selmon->num;
+		}
+#else
 		dmenumon[0] = '0' + selmon->num;
+#endif
 	if (fork() == 0) {
 		if (dpy)
 			close(ConnectionNumber(dpy));
@@ -2054,6 +2101,7 @@ updategeom(void)
 				memcpy(&unique[j++], &info[i], sizeof(XineramaScreenInfo));
 		XFree(info);
 		nn = j;
+		sortscreens(unique, nn);
 		if (n <= nn) { /* new monitors available */
 			for (i = 0; i < (nn - n); i++) {
 				for (m = mons; m && m->next; m = m->next);
