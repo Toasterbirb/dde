a34e6dad1f68a5acaca80eb4b2595b772110122a
Josh Santos <josh@omnidapps.com>
2023-03-06 17:46:53 -0500

    Fix config.def.h formatting

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,8 +1,8 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/													/*Update Interval*/	/*Update Signal*/
-	{" Mem: ", 	"free -h --si | awk '/^Mem/ { print $3\"/\"$2 }'",			30,					0},
-	{"", 		"date '+%b %d (%a) %H:%M%p '",								5,					0},
+	/*Icon*/	/*Command*/	/*Update Interval*/	/*Update Signal*/
+	{" Mem: ", 	"free -h --si | awk '/^Mem/ { print $3\"/\"$2 }'",	30,	0},
+	{"",	"date '+%b %d (%a) %H:%M%p '",	5,	0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
