dfff6288af1528289d2cf39c99161e1342d605f5
Toasterbirb <git@toasterbirb.com>
2023-05-16 20:23:44 +0300

    Remove binaries from the releases + include st

--- release.sh
+++ release.sh
@@ -3,13 +3,21 @@ version=1.0.0
 
 # Create temprary directory for the release
 mkdir -p ./dde
-cp -rv ./dwm ./dwmblocks ./dmenu ./slock ./dde/
+cp -rv ./dwm ./dwmblocks ./dmenu ./slock ./st ./dde/
+
+# Remove binaries
+rm -vf ./dde/dmenu/{dmenu,stest}
+rm -vf ./dde/dwm
+rm -vf ./dde/dwmblocks/dwmblocks
+rm -vf ./dde/slock/slock
+rm -vf ./dde/st/st
 
 # Remove config files
 rm -vf ./dde/dwm/config.h
 rm -vf ./dde/dwmblocks/blocks.h
 rm -vf ./dde/dmenu/config.h
 rm -vf ./dde/slock/config.h
+rm -vf ./dde/st/config.h
 
 # Pack it up
 tar -czvf dde-${version}.tar.gz ./dde
