8e3727c82f8606843ab72555e6b5c04ac0ff2429
toasterbirb <git@toasterbirb.com>
2023-05-20 15:32:50 +0000

    Remove trailing whitespace

--- dmenu/dmenu.c
+++ dmenu/dmenu.c
@@ -64,25 +64,25 @@ static Clr *scheme[SchemeLast];
 static int (*fstrncmp)(const char *, const char *, size_t) = strncmp;
 static char *(*fstrstr)(const char *, const char *) = strstr;
 
-static char**                                                                    
-tokenize(char *source, const char *delim, int *llen) {                           
-   int listlength = 0;                                                           
-   char **list = malloc(1 * sizeof(char*));                                      
-   char *token = strtok(source, delim);                                          
-                                                                                 
-   while (token) {                                                               
-       if (!(list = realloc(list, sizeof(char*) * (listlength + 1))))            
+static char**
+tokenize(char *source, const char *delim, int *llen) {
+   int listlength = 0;
+   char **list = malloc(1 * sizeof(char*));
+   char *token = strtok(source, delim);
+
+   while (token) {
+       if (!(list = realloc(list, sizeof(char*) * (listlength + 1))))
            die("Unable to realloc %d bytes\n", sizeof(char*) * (listlength + 1));
-       if (!(list[listlength] = strdup(token)))                                  
-           die("Unable to strdup %d bytes\n", strlen(token) + 1);                
-       token = strtok(NULL, delim);                                              
-       listlength++;                                                             
-   }                                                                             
-                                                                                 
-   *llen = listlength;                                                           
-   return list;                                                                  
-}                                                                                
-                                                                                 
+       if (!(list[listlength] = strdup(token)))
+           die("Unable to strdup %d bytes\n", strlen(token) + 1);
+       token = strtok(NULL, delim);
+       listlength++;
+   }
+
+   *llen = listlength;
+   return list;
+}
+
 static int
 arrayhas(char **list, int length, char *item) {
    for (int i = 0; i < length; i++) {
